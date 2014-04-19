2265f6a42eaf343e8b83854718bdf1de84234742
tungnlt550@gmail.com <tungnlt550@gmail.com@682ba675-06a4-dd46-b088-98a19df3de9d>
2014-04-19 17:07:00 +0000

    Tinh nang search completed. Anh em coi thu nhe'. Co lenh SQL de insert du lieu test vao table game do'.

---
+++ trunk/database/search/viduAddtoDB.sql
@@ -0,0 +1,6 @@
+USE `flash_game_website`;
+
+INSERT INTO game VALUES(1,'Cut The Rope','jhfg',0,0,'fdhg','jhdf', '2013-11-11');
+INSERT INTO game VALUES(2,'Gold Miner','jhfg',0,0,'fdhg','jhdf', '2013-11-11');
+INSERT INTO game VALUES(3,'How to Have Sex','jhfg',0,0,'fdhg','jhdf', '2013-11-11');
+INSERT INTO game VALUES(4,'How to Have Sex and Cut The Rope and Gold Miner','jhfg',0,0,'fdhg','jhdf', '2013-11-11');
